d248c536f65c5496da138ec95be721638e38d091
SoyeonRyou <ryousoooo@gmail.com>
2025-11-02 10:12:27 +0900

    week6 commit done2

--- week6_gini/app/src/main/java/dduw/com/mobile/week6_gini/HomeFragment.kt
+++ week6_gini/app/src/main/java/dduw/com/mobile/week6_gini/HomeFragment.kt
@@ -2,6 +2,8 @@ package dduw.com.mobile.week6_gini
 
 import android.content.Context
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,12 +13,17 @@ import dduw.com.mobile.week6_gini.album.AlbumFragment
 import dduw.com.mobile.week6_gini.data.TodayMusicData
 import dduw.com.mobile.week6_gini.databinding.FragmentHomeBinding
 import me.relex.circleindicator.CircleIndicator3
+import java.util.Timer
+import kotlin.concurrent.timer
 
 class HomeFragment : Fragment() {
 
     lateinit var binding: FragmentHomeBinding
 
     private lateinit var miniPlayerUpdater: MiniPlayerUpdater
+
+    private val handler = Handler(Looper.getMainLooper())
+    private var timerTask: Timer? = null
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
@@ -46,7 +53,38 @@ class HomeFragment : Fragment() {
         //인디케이터 연결
         indicator.setViewPager(homeViewPager)
 
+        // 자동슬라이드 함수
+        startAutoSlide(bannerAdapter)
+
         return binding.root
+    }
+
+
+    private fun startAutoSlide(adpater : BannerVPAdapter) {
+        val delay = 3000L // 3초 후에 시작
+        val period = 3000L // 3초 간격
+
+        timerTask = timer(period = period) {
+            handler.post {
+                // ViewPager2 ID를 homePannelViewpager2로 통일하여 사용
+                val viewPager = binding.homePannelViewpager2
+
+                // 다음 페이지 인덱스 계산
+                val nextItem = viewPager.currentItem + 1
+
+                if (nextItem < adpater.itemCount) {
+                    viewPager.currentItem = nextItem
+                } else {
+                    viewPager.currentItem = 0 // 마지막 페이지에서 첫 페이지로 순환
+                }
+            }
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        // 타이머의 모든 예약된 작업을 중지+타이머 종료
+        timerTask?.cancel()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
